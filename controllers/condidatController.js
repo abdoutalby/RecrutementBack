32920168ac99010e314e5b43e5ace7e5a1c9dcf1
abdou <abderrahmentalby@gmail.com>
2022-05-31 20:23:26 +0100

    unauthorized

--- controllers/condidatController.js
+++ controllers/condidatController.js
@@ -6,143 +6,146 @@ const Condidat = require("../models/condidatModel");
 // @desc    Register new condidat
 // @route   POST /api/condidats
 // @access  Public
-const registerCondidat = asyncHandler(async(req, res) => {
-    const { name, email, password, tel, adress, status } = req.body;
+const registerCondidat = asyncHandler(async (req, res) => {
+  const { name, email, password, tel, adress, status } = req.body;
 
-    if (!name || !email || !password) {
-        res.status(400);
-        throw new Error("Please add all fields");
-    }
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
 
-    // Check if condidat exists
-    const exists = await Condidat.findOne({ email });
+  // Check if condidat exists
+  const exists = await Condidat.findOne({ email });
 
-    if (exists) {
-        res.status(400);
-        throw new Error("Condidat already exists");
-    }
+  if (exists) {
+    res.status(400);
+    throw new Error("Condidat already exists");
+  }
 
-    // Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-    // Create condidat
-    const condidat = await Condidat.create({
-        name,
-        email,
-        password: hashedPassword,
-        tel,
-        adress,
-        active: req.body.active ? true : false,
+  // Create condidat
+  const condidat = await Condidat.create({
+    name,
+    email,
+    password: hashedPassword,
+    tel,
+    adress,
+    active: req.body.active ? true : false,
+  });
+
+  if (condidat) {
+    res.status(201).json({
+      _id: condidat.id,
+      name: condidat.name,
+      email: condidat.email,
+      adress: condidat.adress,
+      tel: condidat.tel,
+      token: generateToken(condidat._id),
     });
-
-    if (condidat) {
-        res.status(201).json({
-            _id: condidat.id,
-            name: condidat.name,
-            email: condidat.email,
-            adress: condidat.adress,
-            tel: condidat.tel,
-            token: generateToken(condidat._id),
-        });
-    } else {
-        res.status(400);
-        throw new Error("Invalid condidat data");
-    }
+  } else {
+    res.status(400);
+    throw new Error("Invalid condidat data");
+  }
 });
 
-const update = asyncHandler(async(req, res) => {
-    const condidat = req.body;
-    console.log(req.body);
-    await Condidat.findByIdAndUpdate(req.params.id, condidat);
-    const up = await Condidat.findById(req.params.id);
-    res.status(200).json(up);
+const update = asyncHandler(async (req, res) => {
+  const condidat = req.body;
+  console.log(req.body);
+  await Condidat.findByIdAndUpdate(req.params.id, condidat);
+  const up = await Condidat.findById(req.params.id);
+  res.status(200).json(up);
 });
 
-const getById = asyncHandler(async(req, res) => {
-    console.log(req.params.id, "hedha l id ");
-    const condidat = await Condidat.findById(req.params.id);
-    if (!condidat) {
-        res.status(404).json("no condidat with this id ");
-    }
-    res.status(200).json(condidat);
+const getById = asyncHandler(async (req, res) => {
+  console.log(req.params.id, "hedha l id ");
+  const condidat = await Condidat.findById(req.params.id);
+  if (!condidat) {
+    res.status(404).json("no condidat with this id ");
+  }
+  res.status(200).json(condidat);
 });
 
 // @desc    Authenticate a condidat
 // @route   POST /api/condidats/login
 // @access  Public
-const loginCondidat = asyncHandler(async(req, res) => {
-    const { email, password } = req.body;
+const loginCondidat = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-    // Check for user email
-    const condidat = await Condidat.findOne({ email });
-    if (condidat.active) {
-        if (condidat && (await bcrypt.compare(password, condidat.password))) {
-            res.json({
-                _id: condidat.id,
-                name: condidat.name,
-                email: condidat.email,
-                adress: condidat.adress,
-                tel: condidat.tel,
-                token: generateToken(condidat._id),
-            });
-        } else {
-            res.status(400);
-            throw new Error("Invalid credentials");
-        }
+  // Check for user email
+  const condidat = await Condidat.findOne({ email });
+  if (!condidat) {
+    res.status(401).send({ message: "unauthorized" });
+  }else{
+  if (condidat.active) {
+    if (condidat && (await bcrypt.compare(password, condidat.password))) {
+      res.json({
+        _id: condidat.id,
+        name: condidat.name,
+        email: condidat.email,
+        adress: condidat.adress,
+        tel: condidat.tel,
+        token: generateToken(condidat._id),
+      });
     } else {
-        res.status(400);
-        throw new Error("please activate your account");
+      res.status(400);
+      throw new Error("Invalid credentials");
     }
+  } else {
+    res.status(400);
+    throw new Error("please activate your account");
+  }}
 });
 
-const changeStatus = asyncHandler(async(req, res) => {
-    const id = req.params.id;
-    const condidat = await Condidat.findById(id);
-    condidat.active = !condidat.active;
-    await Condidat.findByIdAndUpdate(id, condidat);
+const changeStatus = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const condidat = await Condidat.findById(id);
+  condidat.active = !condidat.active;
+  await Condidat.findByIdAndUpdate(id, condidat);
 
-    res.status(200).json(condidat);
+  res.status(200).json(condidat);
 });
 
 // @desc    delete condidat
 // @route   Delete /api/condidats/
 // @access  Public
-const deleteCondidat = asyncHandler(async(req, res) => {
-    const id = req.params.id;
+const deleteCondidat = asyncHandler(async (req, res) => {
+  const id = req.params.id;
 
-    await Condidat.findByIdAndDelete(id);
-    res.status(200).json("deleted ");
+  await Condidat.findByIdAndDelete(id);
+  res.status(200).json("deleted ");
 });
 // @desc    Get condidat data
 // @route   GET /api/condidats/me
 // @access  Private
-const getCondidat = asyncHandler(async(req, res) => {
-    res.status(200).json(req.user);
+const getCondidat = asyncHandler(async (req, res) => {
+  res.status(200).json(req.user);
 });
 
 // @desc    Get condidats data
 // @route   GET /api/condidats/all
 // @access  Private
-const getCondidats = asyncHandler(async(req, res) => {
-    condidat = await Condidat.find();
-    res.status(200).json(condidat);
+const getCondidats = asyncHandler(async (req, res) => {
+  condidat = await Condidat.find();
+  res.status(200).json(condidat);
 });
 
 // Generate JWT
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: "30d",
-    });
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 };
 
 module.exports = {
-    registerCondidat,
-    loginCondidat,
-    getCondidat,
-    getCondidats,
-    deleteCondidat,
-    changeStatus,
-    getById,
-    update,
+  registerCondidat,
+  loginCondidat,
+  getCondidat,
+  getCondidats,
+  deleteCondidat,
+  changeStatus,
+  getById,
+  update,
 };
--- controllers/recruterController.js
+++ controllers/recruterController.js
@@ -6,140 +6,143 @@ const Recruter = require("../models/recruterModel");
 // @desc    Register new Recruter
 // @route   POST /api/recruters
 // @access  Public
-const registerRecruter = asyncHandler(async(req, res) => {
-    const { name, email, password, tel, adress, status } = req.body;
+const registerRecruter = asyncHandler(async (req, res) => {
+  const { name, email, password, tel, adress, status } = req.body;
 
-    if (!name || !email || !password) {
-        res.status(400);
-        throw new Error("Please add all fields");
-    }
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
 
-    // Check if Recruter exists
-    const exists = await Recruter.findOne({ email });
+  // Check if Recruter exists
+  const exists = await Recruter.findOne({ email });
 
-    if (exists) {
-        res.status(409);
-        throw new Error(" Recruter exists");
-    }
+  if (exists) {
+    res.status(409);
+    throw new Error(" Recruter exists");
+  }
 
-    // Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-    // Create recruter
-    const recruter = await Recruter.create({
-        name,
-        email,
-        password: hashedPassword,
-        status,
-        tel,
-        adress,
+  // Create recruter
+  const recruter = await Recruter.create({
+    name,
+    email,
+    password: hashedPassword,
+    status,
+    tel,
+    adress,
+  });
+
+  if (recruter) {
+    res.status(201).json({
+      _id: recruter.id,
+      name: recruter.name,
+      email: recruter.email,
+      status,
+      token: generateToken(recruter._id),
     });
-
-    if (recruter) {
-        res.status(201).json({
-            _id: recruter.id,
-            name: recruter.name,
-            email: recruter.email,
-            status,
-            token: generateToken(recruter._id),
-        });
-    } else {
-        res.status(400);
-        throw new Error("Invalid recruter data");
-    }
+  } else {
+    res.status(400);
+    throw new Error("Invalid recruter data");
+  }
 });
 
 // @desc    Authenticate a recruter
 // @route   POST /api/recruters/login
 // @access  Public
-const loginRecruter = asyncHandler(async(req, res) => {
-    const { email, password } = req.body;
+const loginRecruter = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-    // Check for recruter email
-    const recruter = await Recruter.findOne({ email });
-
+  // Check for recruter email
+  const recruter = await Recruter.findOne({ email });
+  if (!recruter) {
+    res.status(401).send("unothorized");
+  } else {
     if (!recruter.status) {
-        res.status(400);
-        throw new Error("inactive recruter account");
+      res.status(400);
+      throw new Error("inactive recruter account");
     }
     if (recruter && (await bcrypt.compare(password, recruter.password))) {
-        res.json({
-            _id: recruter.id,
-            name: recruter.name,
-            email: recruter.email,
-            token: generateToken(recruter._id),
-        });
+      res.json({
+        _id: recruter.id,
+        name: recruter.name,
+        email: recruter.email,
+        token: generateToken(recruter._id),
+      });
     } else {
-        res.status(400);
-        throw new Error("Invalid credentials");
+      res.status(401);
+      throw new Error("Invalid credentials");
     }
+  }
 });
 
 // @desc    Get recruter data
 // @route   GET /api/recruters/me
 // @access  Private
-const getAll = asyncHandler(async(req, res) => {
-    recruters = await Recruter.find();
-    res.status(200).json(recruters);
+const getAll = asyncHandler(async (req, res) => {
+  recruters = await Recruter.find();
+  res.status(200).json(recruters);
 });
 
 // @desc    Delete recruter data
 // @route   DELETE /api/recruters/:id
 // @access  Private
-const deleteRecruter = asyncHandler(async(req, res) => {
-    await Recruter.findByIdAndRemove(req.params.id);
-    res.status(200).json("deleted " + req.params.id);
+const deleteRecruter = asyncHandler(async (req, res) => {
+  await Recruter.findByIdAndRemove(req.params.id);
+  res.status(200).json("deleted " + req.params.id);
 });
 
 // @desc    Update recruter status
 // @route   Put /api/recruters/:id
 // @access  Private
-const statusUpdate = asyncHandler(async(req, res) => {
-    const recruter = await Recruter.findById(req.params.id);
-    recruter.status = !recruter.status;
-    await Recruter.findByIdAndUpdate(req.params.id, recruter);
-    const up = await Recruter.findById(req.params.id);
-    res.status(200).json(up);
+const statusUpdate = asyncHandler(async (req, res) => {
+  const recruter = await Recruter.findById(req.params.id);
+  recruter.status = !recruter.status;
+  await Recruter.findByIdAndUpdate(req.params.id, recruter);
+  const up = await Recruter.findById(req.params.id);
+  res.status(200).json(up);
 });
 
-const getById = asyncHandler(async(req, res) => {
-    const recruter = await Recruter.findById(req.params.id);
-    if (!recruter) {
-        res.status(404).json("no recruter with this id ");
-    }
-    res.status(200).json(recruter);
+const getById = asyncHandler(async (req, res) => {
+  const recruter = await Recruter.findById(req.params.id);
+  if (!recruter) {
+    res.status(404).json("no recruter with this id ");
+  }
+  res.status(200).json(recruter);
 });
 
 // @desc    Get recruter data
 // @route   GET /api/recruters/me
 // @access  Private
-const getMe = asyncHandler(async(req, res) => {
-    res.status(200).json(req.user);
+const getMe = asyncHandler(async (req, res) => {
+  res.status(200).json(req.user);
 });
 
 // Generate JWT
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: "30d",
-    });
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 };
 
-const update = asyncHandler(async(req, res) => {
-    const recruter = req.body;
-    console.log(req.body);
-    await Recruter.findByIdAndUpdate(req.params.id, recruter);
-    const up = await Recruter.findById(req.params.id);
-    res.status(200).json(up);
+const update = asyncHandler(async (req, res) => {
+  const recruter = req.body;
+  console.log(req.body);
+  await Recruter.findByIdAndUpdate(req.params.id, recruter);
+  const up = await Recruter.findById(req.params.id);
+  res.status(200).json(up);
 });
 
 module.exports = {
-    registerRecruter,
-    loginRecruter,
-    getMe,
-    getAll,
-    deleteRecruter,
-    statusUpdate,
-    getById,
-    update,
+  registerRecruter,
+  loginRecruter,
+  getMe,
+  getAll,
+  deleteRecruter,
+  statusUpdate,
+  getById,
+  update,
 };
